b757951d540096eafef7a361a9b509bc2d7b10f9
abrarasif11 <faasif929@gmail.com>
2025-08-27 19:29:08 +0600

    create-different-path

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const port = 5000
 app.get('/', (req, res) => {
     res.send('Server Testing 123 Hello!')
   })
-
+  
+  app.get('/data', (req, res) => {
+    res.send('Datas are coming!')
+  }) 
+app.get('/test', (req,res) => {
+    res.send('data testing')
+})
   app.listen(port, () => {
     console.log(`Test Server is running on port ${port}`)
   })
